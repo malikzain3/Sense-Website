8bcce4af1a9f7bae626e61d45c26c05e54d183ff
malik-zain <zainmalik84466@gmail.com>
2026-05-06 11:01:49 +0500

    test push

--- src/Components/RSVPPageRSVPPage.jsx
+++ src/Components/RSVPPageRSVPPage.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 export const RSVPPageRSVPPage = () => {
   return (
     <>
-    <div>
-    <h1>Sinwan is donkey</h1>
-    </div>  
+      <div>
+        <h1>Sinwan is donkey</h1>
+        <h2>yes sinwan is donkey </h2>
+      </div>
     </>
   )
 }
